eb71da247d0d5500d4647e8ea01950c98a1bea45
Adam Zawierucha <zawiedev@gmail.com>
2020-06-29 16:29:43 -0700

    training amount binary search

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import modelHandler
 import dataHandler
 import dnn3
 
-dataHandler.GenerateTrees()
-
+#Generating Testing Data
+dataHandler.Generate("test",1000)
+dataHandler.Generate("dev",100)
 testset = dataHandler.UnpermutedDataset("test")
 valset = dataHandler.UnpermutedDataset("dev")
 
-#Unpermuted Train
-trainset = dataHandler.UnpermutedDataset("train")
-model = dnn3._Model()
-modelHandler.Train(model,trainset,valset,24,name="Zawie's Non-permuted ",doLoad=False)
-accuracy,_ = modelHandler.Test(model,testset,"Test")
+#Binary Search
+amount = 50000
+while True:
+    dataHandler.Generate("train",amount)
+    trainset = dataHandler.UnpermutedDataset("train")
+    model = dnn3._Model()
+    modelHandler.Train(model,trainset,valset,3,name=f"Train Amount = {amount}",doLoad=False)
+    accuracy,_ = modelHandler.Test(model,testset,"Test")
+    if accuracy > .95:
+        amount = amount//2
+    else:
+        amount = int(amount*1.5)
