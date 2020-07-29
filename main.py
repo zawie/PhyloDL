64c5290aac281b665e78ae78d118ae28177d54d6
Adam Zawierucha <zawiedev@gmail.com>
2020-07-28 17:00:51 -0700

    dictionary results, print & plot

--- main.py
+++ main.py
@@ -8,23 +8,30 @@ from plotter import line
 #Run Sequence Length vs. Accuracy Test
 NUM_EPOCHS = 3
 for sL in [20]:
+    #Define results dictionary
+    results = dict()
+
     #Generate Data
     datasets = GenerateDatasets({"train":1000,"test":100,"dev":10},sequenceLength=sL)
 
     #ML Tests
     testset = datasets['test']
     dataset_name = "Simple"
-    IQTREE_accuracy = runIQTREE(dataset_name,testset)
-    RAxML_accuracy = runRAxML(dataset_name,testset)
+    results['IQTREE'] = runIQTREE(dataset_name,testset)
+    results['RAxML (Inference)'] = runRAxML(dataset_name,testset)
+    results['RAxML (Classification)'] = 0
 
     #DL Models Train & Testing
-    res_accuracy = TrainAndTest(dnn3(),datasets,NUM_EPOCHS,f"ResNet: sequenceLength={sL}",doPlot=False)
-    conv_accuracy = TrainAndTest(dnn3NoRes(),datasets,NUM_EPOCHS,f"ConvNet: sequenceLength={sL}",doPlot=False)
+    results['ResNet']  = TrainAndTest(dnn3(),datasets,NUM_EPOCHS,f"ResNet: sequenceLength={sL}",doPlot=False)
+    results['ConvNet']  = TrainAndTest(dnn3NoRes(),datasets,NUM_EPOCHS,f"ConvNet: sequenceLength={sL}",doPlot=False)
 
-    #Print results
-    print(f"Accuracies for sequenceLength={sL}\n\tRes:{res_accuracy}\n\tConv:{conv_accuracy}\n\n\tIQTREE:{IQTREE_accuracy}\nRAxML:{RAxML_accuracy}")
-    #Plot results
-    # TODO: Actually plot this
+    #Print and plot results
+    print(f"Accuracies for sequenceLength={sL}")
+    for name,accuracy in results.items():
+        #Print result
+        print(f"\t{name}: {int(accuracy*100*100)/100}%")
+        #Plot result
+        line(name,[sL],[accuracy],window='Sequence Length vs. Accuracy',xlabel="Sequence Length")
 
     #Save results to CSV
     # TODO: Actually implement this
