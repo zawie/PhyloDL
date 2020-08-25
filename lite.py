1822b0eb50e425e8973b2607f90b944ebc66f932
Adam Zawierucha <zawiedev@gmail.com>
2020-08-25 16:04:06 -0700

    print heatmap

--- lite.py
+++ lite.py
@@ -7,7 +7,7 @@ from evomodels import GTR as getRandomGTRValues
 
 #Run Sequence Length vs. Accuracy Test
 NUM_EPOCHS = 3
-amounts = {"train":10**4,"test":2500,"dev":500}
+amounts = {"train":1000,"test":250,"dev":50}
 sL = 200
 
 #Define results dictionary
@@ -20,7 +20,18 @@ datasets = GenerateDatasets(amounts,sequenceLength=sL)
 # convnet = dnn3()
 # results['ConvNet (dnn3)']  = TrainAndTest(convnet,datasets,NUM_EPOCHS,f"ConvNet",doPlot=True)
 # print(results)
+
+heat_map = {0:0,1:1,2:2}
+dataset = datasets['train']
+for i in range(len(dataset)):
+    for datapoint in dataset[i]:
+        print(datapoint[0])
+        (X,y) = datapoint
+        heat_map[y.to_list()]+=1
+
+print(heat_map)
 print(len(datasets['train']))
+
 resnet = dnn3NoRes()
-results['ResNet (dnn3) w/ Transforms']  = TrainAndTest(resnet,datasets,NUM_EPOCHS,f"ResNet",doPlot=True)
+results['ResNet (dnn3)']  = TrainAndTest(resnet,datasets,NUM_EPOCHS,f"ResNet",doPlot=True)
 print(results)
