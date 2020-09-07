c207361a282d49b8ba27fd6d6d5a7c4b877b284c
Adam Zawierucha <zawiedev@gmail.com>
2020-09-07 11:20:29 -0700

    initial parameter refactor

--- Recombination/generate.py
+++ Recombination/generate.py
@@ -1,4 +1,5 @@
 import Recombination.ctrlGenPar as ctrlGen
+from Recombination.ctrlGenPar import SpeciesTreeInfo
 import Recombination.runINDELible as runINDELible
 import os
 import shutil
@@ -10,24 +11,25 @@ import recombinationPreprocess as recombinationPreprocess
 import recombinationMerge as recombinationMerge
 
 
-def run(speciesTree, recombFactor, seqLen, trialIndex, output):
+def run(speciesTreeInfo, recombFactor, seqLen, trialIndex, output):
+    print("RUN INPUT:",speciesTreeInfo, recombFactor, seqLen, trialIndex, output)
     """
 	Worker thread. Runs one trial of ms and INDELible.
 	"""
 
-    subFolderName, log = ctrlGen.main(speciesTree, seqLen, recombFactor, trialIndex)
+    subFolderName, log = ctrlGen.main(speciesTreeInfo, seqLen, recombFactor, trialIndex)
     log = runINDELible.main(subFolderName, log)
     output.put(log)
 
 
-def main(speciesTree, recombFactor, seqLen, numTrial,doPrint=False):
+def main(speciesTreeInfo, recombFactor, seqLen, numTrial,doPrint=False):
     if doPrint:
         print(
             "=========================================================================================================================")
-        print("[LOG] speciesTree = " + speciesTree + "; recombFactor = " + str(recombFactor) + "; seqLen = " + str(
+        print("[LOG] speciesTree = " + speciesTreeInfo.name + "; recombFactor = " + str(recombFactor) + "; seqLen = " + str(
             seqLen) + "; numTrial = " + str(numTrial))
 
-    folderName = speciesTree + "_" + str(seqLen) + "_" + str(recombFactor) + "_" + str(numTrial)
+    folderName = speciesTreeInfo.name + "_" + str(seqLen) + "_" + str(recombFactor) + "_" + str(numTrial)
     try:
         os.mkdir(folderName)
     except OSError:
@@ -45,7 +47,7 @@ def main(speciesTree, recombFactor, seqLen, numTrial,doPrint=False):
     processes = []
     output = mp.Queue()
     for i in range(numTrial):
-        processes.append(mp.Process(target=run, args=(speciesTree, recombFactor, seqLen, i, output)))
+        processes.append(mp.Process(target=run, args=(speciesTreeInfo, recombFactor, seqLen, i, output)))
 
     for p in processes:
         p.start()
@@ -59,7 +61,10 @@ def main(speciesTree, recombFactor, seqLen, numTrial,doPrint=False):
     os.chdir("..")
     return folderName #required to run recombinationPreprocess
 
-def generate(numDatapoints=1000,treeLabel=2,sequenceLength=1000,recombFactor=1,speciesTree="HCG"):
+HCGInfo = SpeciesTreeInfo(name="HCG",mutationRate=2.5e-6, indelRate=0, defaultRecombRate=1.5e-8, popSize=10000, taxaCount=4,
+                          postR="-I 4 1 1 1 1 -n 1 1.0 -n 2 1.0 -n 3 1.0 -n 4 1.0 -ej 0.5 1 4 -ej 0.5 2 3 -ej 1.0 4 3")
+
+def generate(numDatapoints=1000,treeLabel=2,sequenceLength=1000,recombFactor=1,speciesTreeInfo=HCGInfo):
 
     begin_time = datetime.datetime.now()
     num_trials = 6 #dont make bigger than number of cores (parallel processing)
@@ -70,7 +75,7 @@ def generate(numDatapoints=1000,treeLabel=2,sequenceLength=1000,recombFactor=1,s
 
     for i in range(iterations):
         #generate data
-        data_directory = main(speciesTree=speciesTree, recombFactor=recombFactor, seqLen=sequenceLength, numTrial=num_trials)
+        data_directory = main(speciesTreeInfo=speciesTreeInfo, recombFactor=recombFactor, seqLen=sequenceLength, numTrial=num_trials)
         #recombFactor=1, seqLen: 5000000
 
         #preprocess data
--- lite.py
+++ lite.py
@@ -1,19 +1,22 @@
 from modelHandler import TrainAndTest
 from models import dnn3,dnn3NoRes
 from Recombination.dataHandler import getDataSets
+from Recombination.ctrlGenPar import SpeciesTreeInfo
 from Recombination.generate import generate
 from ML.IQRAX import runIQTREE, runRAxML, runRAxMLClassification
 #Run Sequence Length vs. Accuracy Test
 NUM_EPOCHS = 3
 
 #Generate and get data
-generate(numDatapoints=200,treeLabel=2,sequenceLength=1000)
+HCGInfo = SpeciesTreeInfo(name="HCG",mutationRate=2.5e-6, indelRate=0, defaultRecombRate=1.5e-8, popSize=10000, taxaCount=4,
+                          postR="-I 4 1 1 1 1 -n 1 1.0 -n 2 1.0 -n 3 1.0 -n 4 1.0 -ej 0.5 1 4 -ej 0.5 2 3 -ej 1.0 4 3")
+
+generate(numDatapoints=10,treeLabel=2,sequenceLength=1000,recombFactor=1,speciesTreeInfo=HCGInfo)
 datasets = getDataSets(0)
 
 #Train and test model
 # resnet = dnn3NoRes()
 # accuracy = TrainAndTest(resnet,datasets,NUM_EPOCHS,f"Recombination_ResNet",doPlot=True)
 # print("MODEL ACCURACY:",accuracy)
-
 
 print("IQTREE ACCURACY:",runIQTREE(datasets['train']))
