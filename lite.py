102cffeed8032ab933f4df38894a3cafdf75d832
Adam Zawierucha <zawiedev@gmail.com>
2020-09-21 10:35:29 -0700

    Xinhao Tree

--- lite.py
+++ lite.py
@@ -6,25 +6,13 @@ from ML.IQRAX import runIQTREE, runRAxML, runRAxMLClassification
 
 import math
 
-# sequenceLength = 10000
-# numDatapoints = 250
-
-# for mR in [2.5e-8,2.5e-7,2.5e-6]:
-#     HCGInfo = SpeciesTreeInfo(name="HCG",mutationRate=mR, indelRate=0, defaultRecombRate=1.5e-8, popSize=10000, taxaCount=4,
-#                                postR="-I 4 1 1 1 1 -n 1 1.0 -n 2 1.0 -n 3 1.0 -n 4 1.0 -ej 0.5 1 4 -ej 1.0 4 2 -ej 1.5 2 3")
-#     for rF in [1,2,3,4,5,6,7,8,9,10]:
-#         datasets = generateAndGet(numDatapoints=numDatapoints,treeLabel=2,sequenceLength=sequenceLength,recombFactor=rF,speciesTreeInfo=HCGInfo)
-#         accuracy = runIQTREE(datasets['train'])
-#         line(f"Mutation Rate = {mR}",[rF],[accuracy],window=f'Accuracy v. Recombo Factor | length={sequenceLength}, count={numDatapoints}',xlabel="Recombo Factor")
-#         print(f"RecomboFactor={rF}\t MutationRate={mR}\t Accuracy={accuracy}")
-
 numTrials = 100
-for sL in [100,1000,10000,100000]:
-    for mR in [2.5e-8,2.5e-7,2.5e-6]:
-        HCGInfo = SpeciesTreeInfo(name="HCG",mutationRate=mR, indelRate=0, defaultRecombRate=1.5e-8, popSize=10000, taxaCount=4,
-                                   postR="-I 4 1 1 1 1 -n 1 1.0 -n 2 1.0 -n 3 1.0 -n 4 1.0 -ej 0.5 1 4 -ej 0.5 2 3 -ej 1.0 4 3")
+for sL in [int(1e2),int(1e3),int(1e4),int(1e5),int(1e6)]:
+    for mR in [1.25e-8,1.25e-7,1.25e-6]:
+        prStr ="-I 4 1 1 1 1 -n 2 1.0 -n 3 1.0 -n 1 1.0 -n 4 1.0 -ej 2.5 1 2 -en 2.5 2 4.0 -ej 4.0 2 3 -en 4.0 3 4.0 -ej 11.25 3 4 -en 11.25 4 4.0"
+        XinhaoInfo= SpeciesTreeInfo(name = 'XinhaoTree', mutationRate=mR, indelRate=0, defaultRecombRate=1.5e-7, popSize=10000, taxaCount=4,postR = prStr)
         for rF in [0]:
-            datasets = generateAndGet(numDatapoints=numTrials,treeLabel=2,sequenceLength=sL,recombFactor=rF,speciesTreeInfo=HCGInfo)
+            datasets = generateAndGet(numDatapoints=numTrials,treeLabel=2,sequenceLength=sL,recombFactor=rF,speciesTreeInfo=XinhaoInfo)
             accuracy = runIQTREE(datasets['train'])
             line(f"Mutation Rate: {mR}",[math.ceil(math.log(sL, 10))],[accuracy],window=f'Accuracy v. Mutation Rate| count={numTrials}, recomboFactor={rF}',xlabel="Sequence Length - logbase 10")
             #line(f"Mutation Rate = {mR}",[rF],[accuracy],window=f'Accuracy v. Recombo Factor | length={sequenceLength}, count={numTrials}',xlabel="Recombo Factor")
