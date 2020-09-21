2bd7046eecb2c1b7aac085a1c5c6d0dfecdce87e
Adam Zawierucha <zawiedev@gmail.com>
2020-09-21 11:15:12 -0700

    tree label fix (to alpha, 0), fixed transformations

---
+++ Recombination/transformations.py
@@ -0,0 +1,70 @@
+import torch
+toXTensor = lambda sequence: torch.tensor(sequence,dtype=torch.float)
+toYTensor = lambda n: torch.tensor(n,dtype=torch.long)
+
+#Alpha conveteres
+def alphaToGamma(alphaSeqeunces):
+    """
+    Transforms a given alpha sequences to a beta tree
+    """
+    (A,B,C,D) = alphaSeqeunces
+    return [A,D,C,B]
+
+def alphaToBeta(alphaSeqeunces):
+    """
+    Transforms a given alpha sequences to a gamma tree
+    """
+    (A,B,C,D) = alphaSeqeunces
+    return [A,C,B,D]
+
+#Beta converters
+def betaToAlpha(betaSequences):
+    """
+    Transforms a given beta sequences to a alpha tree
+    """
+    (A,C,B,D) = betaSequences
+    return [A,B,C,D]
+
+def betaToGamma(betaSequences):
+    """
+    Transforms a given beta sequences to a gamma tree
+    """
+    (A,C,B,D) = betaSequences
+    return [A,D,C,B]
+
+#Gamma converters
+def gammaToAlpha(gammaSequences):
+    """
+    Transforms a given gamma sequences to an alpha tree
+    """
+    (A,D,C,B) = gammaSequences
+    return [A,B,C,D]
+
+def gammaToBeta(gammaSequences):
+    """
+    Transforms a given gamma sequences to a beta tree
+    """
+    (A,D,C,B)= gammaSequences
+    return [A,C,B,D]
+
+#Meta-converters
+def transformAlpha(alphaSequences):
+    return [alphaSequences,alphaToBeta(alphaSequences),alphaToGamma(alphaSequences)]
+def transformBeta(betaSequences):
+    return [betaToAlpha(betaSequences),betaSequences,betaToGamma(betaSequences)]
+def transformGamma(gammaSequences):
+    return [gammaToAlpha(gammaSequences),gammaToBeta(gammaSequences),gammaSequences]
+
+def transformSequences(tensorSequences,label):
+    #Tensor -> List
+    label = label.tolist()
+    #Perform switch
+    switch = {0:transformAlpha,1:transformBeta,2:transformGamma}
+    listOfSequenceSets = switch[label](sequenceSet)
+    #List -> tensor
+    tensorSequenceSets = list()
+    for sequenceSet in listOfSequenceSets:
+        tensorSequenceSets.append(toXTensor(sequenceSet))
+    #Labels
+    labels = [toYTensor(0),toYTensor(1),toYTensor(2)]
+    return (tensorSequenceSets,labels)
--- lite.py
+++ lite.py
@@ -9,11 +9,14 @@ import math
 numTrials = 100
 for sL in [int(1e2),int(1e3),int(1e4),int(1e5),int(1e6)]:
     for mR in [1.25e-8,1.25e-7,1.25e-6]:
+        #Tree structure information
+        label = 0
         prStr ="-I 4 1 1 1 1 -n 2 1.0 -n 3 1.0 -n 1 1.0 -n 4 1.0 -ej 2.5 1 2 -en 2.5 2 4.0 -ej 4.0 2 3 -en 4.0 3 4.0 -ej 11.25 3 4 -en 11.25 4 4.0"
+        #SpeciesTree info
         XinhaoInfo= SpeciesTreeInfo(name = 'XinhaoTree', mutationRate=mR, indelRate=0, defaultRecombRate=1.5e-7, popSize=10000, taxaCount=4,postR = prStr)
         for rF in [0]:
-            datasets = generateAndGet(numDatapoints=numTrials,treeLabel=1,sequenceLength=sL,recombFactor=rF,speciesTreeInfo=XinhaoInfo)
+            #Generate data
+            datasets = generateAndGet(numDatapoints=numTrials,treeLabel=label,sequenceLength=sL,recombFactor=rF,speciesTreeInfo=XinhaoInfo)
+            #Compute accuacy using ML
             accuracy = runIQTREE(datasets['train'])
             line(f"Mutation Rate: {mR}",[math.ceil(math.log(sL, 10))],[accuracy],window=f'Accuracy v. Mutation Rate| count={numTrials}, recomboFactor={rF}',xlabel="Sequence Length - logbase 10")
-            #line(f"Mutation Rate = {mR}",[rF],[accuracy],window=f'Accuracy v. Recombo Factor | length={sequenceLength}, count={numTrials}',xlabel="Recombo Factor")
-            print(f"RecomboFactor={rF}\t MutationRate={mR}\t Accuracy={accuracy}")
