8fe45600c1f31a475751501f84f769ccb2a55aa4
Adam Zawierucha <zawiedev@gmail.com>
2020-11-30 16:05:38 -0800

    DL test

--- lite.py
+++ lite.py
@@ -3,17 +3,18 @@ from util.plotter import line
 from models import dnn3
 from Recombination.main import generateData
 from ML.IQRAX import runIQTREE, runRAxML, runRAxMLClassification
+import time
 
 #Generate data
 datasets = generateData()
-#Compute accuacy using ML
-MLaccuracy = runIQTREE(datasets['train'])
-
 #Compute accuray using DL
-#DLaccuracy = TrainAndTest(dnn3(),datasets,10,f"ConvNet",doPlot=True)
+DLaccuracy = TrainAndTest(dnn3(),datasets,10,f"ConvNet",doPlot=True)
+
+#Compute accuacy using ML
+MLaccuracy = runIQTREE(datasets['test'])
 
 #Compute and print
 print("-"*100+"\n ML Accuracy:")
 print(MLaccuracy)
 print("\n DL Accuracy:")
-#print(DLaccuracy)
+print(DLaccuracy)
