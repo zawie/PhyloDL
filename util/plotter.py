d0f36691877d260024fe63458272d804c17a3d40
Adam Zawierucha <zawiedev@gmail.com>
2020-12-11 00:12:42 -0800

    scatter plot

--- util/plotter.py
+++ util/plotter.py
@@ -2,13 +2,13 @@ import visdom
 import numpy as np
 
 #Connect to Visdom
-viz = visdom.Visdom(env='main')
+vis = visdom.Visdom(env='main')
 
 def line(name,X,Y,window='Main',xlabel="Epoch"):
     """
     Plots onto a visdom plot
     """
-    viz.line(
+    vis.line(
         X=np.array(X),
         Y=np.array(Y),
         win=window,
@@ -19,10 +19,23 @@ def line(name,X,Y,window='Main',xlabel="Epoch"):
 
 def heatmap(name, X, window="Main",xlabel="X-Axis",ylabel="Y-Axis"):
     """
-    Plots a visdom scatter plot
+    Plots a visdom heatmap
     """
-    viz.heatmap(
+    vis.heatmap(
         X=np.array(X),
         win=window,
         opts = dict(columnnames=xlabel, rownames=ylabel)
+    )
+
+def scatter(name, X, Y, window="Main", xlabel="X-Axis", ylabel="Y-Axis"):
+    """
+    Plots a visdom scatter plot
+    """
+    vis.scatter(
+        X=np.array(X),
+        Y=np.array(Y),
+        win=window,
+        name=name,
+        update='append',
+        opts = dict(title=window, xlabel=xlabel, showlegend=True)
     )
