d27b39c5868617a36b04d103a1139aa43d772cb0
Adam Zawierucha <zawiedev@gmail.com>
2020-12-11 18:25:23 -0800

    zawie tweaks

--- zawie.py
+++ zawie.py
@@ -1,20 +1,17 @@
 from modelHandler import TrainAndTest
-from Recombination.dataHandler import splitDatasets, loadDataset
+from Recombination.dataHandler import splitDatasets, loadDataset, saveDataset
 from util.plotter import line
 from models import dnn3
 from Recombination.main import generateData
 from ML.IQRAX import runIQTREE, runRAxML, runRAxMLClassification
 import time
 
-
-#dataset = generateData(name="Ryan",amountOfTrees=10, numTrials=10, anomolyOnly=True)
-
 #Generate data
 # dataset = generateData(name="Alpha10k",amountOfTrees=1000, numTrials=10)
 # dataset = generateData(name="Alpha50k",amountOfTrees=2500, numTrials=20)
 # dataset = generateData(name="Alpha100k",amountOfTrees=5000, numTrials=40)
 
-dataset = loadDataset("Alpha10k")
+dataset = loadDataset("Alpha50k")
 datasets = splitDatasets(dataset,setProbabilities = [80, 5, 15])
 
 # #Compute accuacy using ML
