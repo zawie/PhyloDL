75823659874c8cbb55b71eeb17947668646c7493
Ryan Huckleberry <ryanthuckleberry@gmail.com>
2020-12-12 11:09:23 -0800

    anomoly plotter

--- anomolyZone/anomolyDist.py
+++ anomolyZone/anomolyDist.py
@@ -1,4 +1,5 @@
 import numpy as np
+import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import checkAnomolyBL
 
@@ -27,26 +28,52 @@ def anomolyDist(anomolyX, anomolyY, name="Anomoly Zone", anomolyApprox=checkAnom
     - anomolyApprox: function of anomoly approximation
     Ouput: Graph of distribution of anomoly zone data (x,y) branch lengths
     """
-    print("[PLOTTING]")
-    #anomoly graph details
-    plt.title(name)
-    plt.xlabel("x Branch Length")
-    plt.ylabel("y Branch Length")
-    # plt.xlim(0, 0.27)
-    # plt.ylim(0, 1.1)
 
-    #anomoly zone function approximation plot
+    #generate more datapoints in anomolyzone
     x = np.linspace(start=0, stop = 0.27, num=1000)
     y = anomolyApprox(x)
-    plt.plot(x, y, c="darkblue")
-    plt.fill_between(x,y,color="lightblue")
 
-    #graph anomoly data points
-    plt.scatter(anomolyX, anomolyY, c='red',marker=".", s=30)
+    #graph range
+    x_range = [0, max(max(anomolyX), max(x))]
+    y_range = [0, max(max(anomolyY), max(y))]
+
+    #build figure
+    fig = go.Figure()
+    fig.update_layout(title=name, title_x=0.25, title_y=0.85,
+                    xaxis_title="x Branch Length",yaxis_title="y Branch Length",
+                    xaxis_range=x_range,yaxis_range=y_range)
+
+    fig.add_trace(go.Scatter(x=x, y=y, mode='lines',fill='tozeroy',
+                            name = 'Anomoly Zone Approximation'))
+    fig.add_trace(go.Scatter(x=anomolyX, y=anomolyY,
+                            mode='markers',name='Anomoly Datapoints'))
+
+    #fig.show()
 
     #save figure
-    plt.savefig("anomolyZone/distPlots/" + name)
-    plt.close('all')
+    fig.write_image("anomolyZone/distPlots/" + name + '.png')
+
+
+    # print("[PLOTTING]")
+    # #anomoly graph details
+    # plt.title(name)
+    # plt.xlabel("x Branch Length")
+    # plt.ylabel("y Branch Length")
+    # # plt.xlim(0, 0.27)
+    # # plt.ylim(0, 1.1)
+    #
+    # #anomoly zone function approximation plot
+    # x = np.linspace(start=0, stop = 0.27, num=1000)
+    # y = anomolyApprox(x)
+    # plt.plot(x, y, c="darkblue")
+    # plt.fill_between(x,y,color="lightblue")
+    #
+    # #graph anomoly data points
+    # plt.scatter(anomolyX, anomolyY, c='red',marker=".", s=30)
+    #
+    # #save figure
+    # plt.savefig("anomolyZone/distPlots/" + name)
+    # plt.close('all')
 
 if __name__ == "__main__":
     anomolyX = [0.26,0.025,0.05,0.1,0.2]
