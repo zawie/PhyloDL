504e3645c804d84b33c461a5f39255e9c280e6a4
Adam Zawierucha <zawiedev@gmail.com>
2020-12-12 12:36:13 -0800

    ryan ml RUN

--- ryan.py
+++ ryan.py
@@ -6,4 +6,14 @@ from Recombination.main import generateData
 from ML.IQRAX import runIQTREE, runRAxML, runRAxMLClassification
 import time
 
-dataset = generateData(name="Ryan",amountOfTrees=10, numTrials=10, anomolyOnly=True)
+dataset = generateData(name="RyanAnomaly",amountOfTrees=10, numTrials=10, anomolyOnly=True)
+IQAcc = runIQTREE(dataset)
+RaxAcc = runRAxML(dataset)
+RaxClassAcc = runRAxMLClassification(dataset)
+
+print("-"*100+"\n IQ Accuracy:\n")
+print(IQAcc)
+print("-"*100+"\n RAxML Accuracy:\n")
+print(RaxAcc)
+print("-"*100+"\n RAxML Accuracy:\n")
+print(RaxClassAcc)
