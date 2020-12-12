66ef6c0ca1c3dda8caea1efd968502691357fd89
Adam Zawierucha <zawiedev@gmail.com>
2020-12-12 12:33:22 -0800

    zawie tweaks

--- zawie.py
+++ zawie.py
@@ -6,13 +6,15 @@ from Recombination.main import generateData
 from ML.IQRAX import runIQTREE, runRAxML, runRAxMLClassification
 import time
 
+#dataset = generateData(name="Alpha10k",amountOfTrees=1000, numTrials=10)
+
 #Generate data
 # dataset = generateData(name="Alpha10k",amountOfTrees=1000, numTrials=10)
 # dataset = generateData(name="Alpha50k",amountOfTrees=2500, numTrials=20)
 # dataset = generateData(name="Alpha100k",amountOfTrees=5000, numTrials=40)
 
 dataset = loadDataset("Alpha50k")
-datasets = splitDatasets(dataset,setProbabilities = [80, 5, 15])
+datasets = splitDatasets(dataset,setProbabilities = [846, 4, 150])
 
 # #Compute accuacy using ML
 MLaccuracy = runIQTREE(datasets['test'])
@@ -20,7 +22,7 @@ print("-"*100+"\n ML Accuracy:")
 print(MLaccuracy)
 
 #Compute accuray using DL
-DLaccuracy = TrainAndTest(dnn3(),datasets,3,f"ConvNet",doPlot=True)
+DLaccuracy = TrainAndTest(dnn3(),datasets,3,f"-ConvNet-",doPlot=True)
 
 #Compute and print
 print("-"*100+"\n ML Accuracy:")
