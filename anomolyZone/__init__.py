9a141ff741df8acb13dae3f06c59204633c4a566
Adam Zawierucha <zawiedev@gmail.com>
2020-12-26 18:30:56 -0800

    removes anomaly zone tracker

--- anomolyZone/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-#Get information
-folderName = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
-baseDirectory = os.getcwd()
-#Append to system pathes
-sys.path.append(f"{baseDirectory}/{folderName}")
